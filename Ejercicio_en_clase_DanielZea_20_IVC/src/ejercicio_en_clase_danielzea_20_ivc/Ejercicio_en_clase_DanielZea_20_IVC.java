87e0726d2702b4d137ff3fd2c0e2756437fc6d45
26ALZ01-DanielZea <lopezalvaro16db@gmail.com>
2026-08-17 13:48:47 -0600

    Add files via upload

---
+++ Ejercicio_en_clase_DanielZea_20_IVC/src/ejercicio_en_clase_danielzea_20_ivc/Ejercicio_en_clase_DanielZea_20_IVC.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio_en_clase_danielzea_20_ivc;
+
+/**
+ *
+ * @author LABORATORIO
+ */
+public class Ejercicio_en_clase_DanielZea_20_IVC {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Agenda b = new Agenda();
+        b.setVisible(true);
+    }
+    
+}
